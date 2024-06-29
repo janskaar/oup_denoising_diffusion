d8602f70fdb475423be700734fcb482c3441ce26
janskaar <jewskaar@gmail.com>
2024-06-29 14:39:21 +0200

    run test set simulations

--- run_simulations.py
+++ run_simulations.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy import stats
 import os, time
 
+np.random.seed(123)
+
 prior_min = np.array([1, 1, 1, -250])
 prior_max = np.array([10, 10, 10, 250])
 scale = prior_max - prior_min
@@ -12,31 +14,33 @@ num_procs = 1
 
 z0 = np.zeros((num_procs, 2))
 
-num_sims = 10000
+num_sims = 2000
+param_vals = prior.rvs(size=(num_sims, 4))
+
+# # #  For running multiple simulations at same parameter values
+# param_vals = prior.rvs(size=(9, 4))
+# param_vals = np.tile(param_vals, (50, 1))
+# param_vals = param_vals.reshape((50, 9, 4)).transpose(1,0,2).reshape((450,4))
 
 zs = []
-thetas = []
 ## Simulations
-for i in range(num_sims):
+for i, _ in enumerate(param_vals):
     print(i, end="\r")
-    param_vals = prior.rvs()
     params = SimulationParameters(
         num_procs=num_procs,
-        sigma_noise=param_vals[0],
-        tau_x=param_vals[1],
-        tau_y=param_vals[2],
-        C=param_vals[3],
+        sigma_noise=param_vals[i,0],
+        tau_x=param_vals[i,1],
+        tau_y=param_vals[i,2],
+        C=param_vals[i,3],
     )
 
     simulator = ParticleSimulator(z0, params)
     simulator.simulate(3000)
-    thetas.append(param_vals)
     zs.append(simulator.z.squeeze())
 
-thetas = np.array(thetas)
 zs = np.array(zs)
 
 zs = zs[:, :30000:10, :]
 
-np.save(os.path.join("data", "theta.npy"), thetas)
-np.save(os.path.join("data", "z.npy"), zs)
+np.save(os.path.join("data", "theta_test_set.npy"), param_vals)
+np.save(os.path.join("data", "z_test_set.npy"), zs)
