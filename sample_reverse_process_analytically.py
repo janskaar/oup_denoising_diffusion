13a3d4085f04609535e3e51e9731d5f74501e46c
janskaar <jewskaar@gmail.com>
2024-07-11 12:20:46 +0200

    fix bug, works now

--- sample_reverse_process_analytically.py
+++ sample_reverse_process_analytically.py
@@ -92,49 +92,49 @@ delta_s = jnp.arange(1024)
 
 ##
 
-@partial(jax.vmap, in_axes=(0, 0, None, None, None))
-def forward_posterior_sample(key, x_t, alpha_bar_t, beta_tp1, sigma_OU):
-    n = 2048
-    sigma_q_t = 1 - alpha_bar_t
-    t1_inverse = jnp.linalg.solve(alpha_bar_t * sigma_OU + sigma_q_t * jnp.eye(n), jnp.eye(n))
-
-    Sigma_inv = t1_inverse + (1 - beta_tp1) / beta_tp1 * jnp.eye(n)
-
-    chol_Sigma_inv = jnp.linalg.cholesky(Sigma_inv, upper=False)
-    chol_Sigma = jsp.linalg.solve_triangular(chol_Sigma_inv, jnp.eye(n), lower=True)
-    Sigma = chol_Sigma.T.dot(chol_Sigma)
-    mu = np.sqrt(1 - beta_tp1) / beta_tp1 * Sigma.dot(x_t)
-
-    sample = chol_Sigma.dot(jax.random.normal(key, shape=x_t.shape)) + mu
-    return sample
-
-rng = jax.random.PRNGKey(123)
-rng, key = jax.random.split(rng)
-# sample at step T
-sample = jax.random.normal(key, shape=(10000, 2048))
-samples = [sample]
-cov_OU = compute_ou_covariance(delta_s, params)
-for i in reversed(range(999)):
-    tic = time.time()
-
-    rng, key = jax.random.split(rng)
-    keys = jax.random.split(key, len(sample))
-
-    sample = forward_posterior_sample(keys, sample, ddpm_params["alphas_bar"][i], ddpm_params["betas"][i+1], cov_OU)
-    if ( i % 100 == 0 ):
-        samples.append(sample)
-    toc = time.time()
-    print(f"step {i}, {toc - tic:.1f} s")
-
-samples = np.array(samples)
-
-with h5py.File("reverse_process_analytical_samples.h5", "a") as f:
-    f.create_dataset("samples", data=samples)
-
-    f.attrs["sigma_noise"] = 5.
-    f.attrs["tau_x"] = 4.
-    f.attrs["tau_y"] = 5.
-    f.attrs["C"] = 50.
+# @partial(jax.vmap, in_axes=(0, 0, None, None, None))
+# def forward_posterior_sample(key, x_t, alpha_bar_t, beta_tp1, sigma_OU):
+#     n = 2048
+#     sigma_q_t = 1 - alpha_bar_t
+#     t1_inverse = jnp.linalg.solve(alpha_bar_t * sigma_OU + sigma_q_t * jnp.eye(n), jnp.eye(n))
+# 
+#     Sigma_inv = t1_inverse + (1 - beta_tp1) / beta_tp1 * jnp.eye(n)
+# 
+#     chol_Sigma_inv = jnp.linalg.cholesky(Sigma_inv, upper=False)
+#     chol_Sigma = jsp.linalg.solve_triangular(chol_Sigma_inv, jnp.eye(n), lower=True)
+#     Sigma = chol_Sigma.T.dot(chol_Sigma)
+#     mu = np.sqrt(1 - beta_tp1) / beta_tp1 * Sigma.dot(x_t)
+# 
+#     sample = chol_Sigma.dot(jax.random.normal(key, shape=x_t.shape)) + mu
+#     return sample
+# 
+# rng = jax.random.PRNGKey(123)
+# rng, key = jax.random.split(rng)
+# # sample at step T
+# sample = jax.random.normal(key, shape=(10000, 2048))
+# samples = [sample]
+# cov_OU = compute_ou_covariance(delta_s, params)
+# for i in reversed(range(999)):
+#     tic = time.time()
+# 
+#     rng, key = jax.random.split(rng)
+#     keys = jax.random.split(key, len(sample))
+# 
+#     sample = forward_posterior_sample(keys, sample, ddpm_params["alphas_bar"][i], ddpm_params["betas"][i+1], cov_OU)
+#     if ( i % 100 == 0 ):
+#         samples.append(sample)
+#     toc = time.time()
+#     print(f"step {i}, {toc - tic:.1f} s")
+# 
+# samples = np.array(samples)
+# 
+# with h5py.File("reverse_process_analytical_samples.h5", "a") as f:
+#     f.create_dataset("samples", data=samples)
+# 
+#     f.attrs["sigma_noise"] = 5.
+#     f.attrs["tau_x"] = 4.
+#     f.attrs["tau_y"] = 5.
+#     f.attrs["C"] = 50.
 
 ## 
 
@@ -149,27 +149,27 @@ def compute_temporal_covariances(x0, xt):
     return (x0[:,:,None] * xt[:,None,:]).mean(0)
 
 ts = list(range(0, 1000, 100)) + [999]
-
-for i, sample in enumerate(samples):
-    sample = sample.reshape((-1, 1024, 2))
+i = 0 
+for i, sample in enumerate(reversed(samples)):
+    sample = sample.reshape((-1, 2, 1024)).transpose(0,2,1)
     cov_sample = compute_temporal_covariances(sample[:,0,:], sample)
     cov_analytical = compute_ou_cov_diffusion_t(ddpm_params["alphas_bar"][ts[i]], delta_s, params)
     fig, ax = plt.subplots(2, 2, sharex=True)
     fig.set_size_inches(10,10)
-
+    
     ax[0,0].plot(cov_sample[:,0,0], label="simulation")
     ax[0,0].plot(cov_analytical[0,:1024], label="theory")
     ax[0,0].legend()
-
+    
     ax[0,1].plot(cov_sample[:,0,1])
     ax[0,1].plot(cov_analytical[0,1024:])
-
+    
     ax[1,0].plot(cov_sample[:,1,0])
     ax[1,0].plot(cov_analytical[1024,:1024])
-
+    
     ax[1,1].plot(cov_sample[:,1,1])
     ax[1,1].plot(cov_analytical[1024,1024:])
-
+    
     ax[0,0].set_xlim(0, 50)
 
     fig.savefig(os.path.join("figures_reverse_process", f"reverse_process_step_{ts[i]}.png"))
