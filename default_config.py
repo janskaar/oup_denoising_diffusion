98f5f967b2118c5fd436ede5ceb4a0740df52b10
janskaar <jewskaar@gmail.com>
2024-07-19 16:21:33 +0200

    rm attention option

--- default_config.py
+++ default_config.py
@@ -39,7 +39,6 @@ model.filter_mults = (1, 2, 4, 8)
 model.encoder_start_filters = 16
 model.encoder_filter_mults = (1, 2, 4, 8)
 model.encoder_latent_dim = 4
-model.use_attention = False
 
 # optim
 config.optim = optim = ml_collections.ConfigDict()
